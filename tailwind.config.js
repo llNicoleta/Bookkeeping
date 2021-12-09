47202b4aa51e83068588d87fdf046bb3e98a2dd9
Nicoleta Panaghiu <idlnicoleta@gmail.com>
2021-12-09 15:06:20 +0200

    - added tailwind support

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  prefix: '',
+  purge: [
+    './src/**/*.{html,ts}',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
